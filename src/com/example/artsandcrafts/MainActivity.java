184da0ebee172e7e26812c1ae8167736617edf11
Phil Godzin <pgg2105@columbia.edu>
2014-10-18 23:44:39 -0400

    main activity is back

--- src/com/example/artsandcrafts/MainActivity.java
+++ src/com/example/artsandcrafts/MainActivity.java
@@ -1,12 +1,66 @@
 package com.example.artsandcrafts;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        String[] categories = getResources().getStringArray(R.array.categories);
+        ListView lv = getListView();
+        lv.setCacheColorHint(0);
+        lv.setBackgroundResource(R.drawable.bg);
+        setListAdapter(new ImageAdapter(this, categories));
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Activity activity;
+        private LayoutInflater inflater;
+        private String[] categories;
+
+        public ImageAdapter(Activity activity, String[] categories) {
+            this.activity = activity;
+            this.categories = categories;
+        }
+
+        @Override
+        public int getCount() {
+            return categories.length;
+        }
+
+        @Override
+        public Object getItem(int location) {
+            return categories[location];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (inflater == null)
+                inflater = (LayoutInflater) activity
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null)
+                convertView = inflater.inflate(R.layout.list_item, null);
+
+            TextView name = (TextView) convertView.findViewById(R.id.category);
+            name.setText(categories[position]);
+
+            return convertView;
+        }
+
     }
 }
